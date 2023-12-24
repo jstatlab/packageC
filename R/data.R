9a02602d5957766e597f2aa3b4d60ae86796c069
jstatlab <jstatlab@gmail.com>
2023-12-24 17:04:27 -0300

    Add data

---
+++ R/data.R
@@ -0,0 +1,24 @@
+#' @title Data of motor trend car road tests
+#'
+#' @description
+#' The data was extracted from the 1974 Motor Trend US magazine, and comprises
+#' fuel consumption and 10 aspects of automobile design and performance for
+#' 32 automobiles (1973–74 models).
+#'
+#' @format A data frame with 32 rows and 11 variables
+#'
+#' \describe{
+#'  \item{mpg}{Miles/(US) gallon}
+#'  \item{cyl}{Number of cylinders}
+#'  \item{disp}{Displacement}
+#'  \item{hp}{Gross horsepower}
+#'  \item{drat}{Rear axle ratio}
+#'  \item{wt}{Weight (1000 lbs)}
+#'  \item{qsec}{1/4 mile time}
+#'  \item{vs}{Engine (0 = V-shaped, 1 = straight)}
+#'  \item{am}{	Transmission (0 = automatic, 1 = manual)}
+#'  \item{gear}{Number of forward gears}
+#'  \item{carb}{Number of carburetors}
+#' }
+#' @source \url{https://www.rdocumentation.org/packages/datasets/versions/3.6.2/topics/mtcars}
+"mtcars"
---
+++ data-raw/mtcars.R
@@ -0,0 +1,4 @@
+## code to prepare `mtcars` dataset goes here
+
+mtcars <- mtcars
+usethis::use_data(mtcars, overwrite = TRUE)
